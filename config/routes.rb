40fb18ce2c95989a6202b739922d08d09ef6c3d6
Joe Sherman <jbsherman365@gmail.com>
2019-10-09 13:03:20 -0400

    config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'movies/good'
-  get 'movies/bad'
+  get 'movies/good' => 'movies#good'
+  get 'movies/bad' => 'movies#bad'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
